8ffa3dcaec63ae1b216556912bf532118089db00
Matthias Sunday Oduh <sundayoduhmatthias@gmail.com>
2023-10-07 16:34:16 +0000

    some tweak

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,24 +8,49 @@
  */
 int main(void)
 {
-	unsigned int a, b, c, i;
+	unsigned long int a, b, c, i;
+	unsigned long int a_h1, a_h2, b_h1, b_h2, h1, h2;
 
 	a = 0;
 	b = 1;
 	c = a + b;
-	printf("%u, ", c);
-	for (i = 2; i <= 98; i++)
+	printf("%lu, ", c);
+	for (i = 2; i < 93; i++)
 	{
 		a = b;
 		b = c;
 		c = a + b;
-		printf("%u", c);
-		if (i == 98)
-			continue;
+		printf("%lu", c);
 		putchar(',');
 		putchar(' ');
 	}
+
+	a_h1 = b / 1000000;
+	a_h2 = b % 1000000;
+	b_h1 = c / 1000000;
+	b_h2 = c % 1000000;
+
+	for (i = 93; i < 99; i++)
+	{
+		h1 = a_h1 + b_h1;
+		h2 = a_h2 + b_h2;
+		if (h2 > 9999999)
+		{
+			h1 = h1 + 1;
+			h2 = h2 % 1000000;
+		}
+		printf("%lu%lu", h1, h2);
+		if (i == 98)
+			continue;
+		putchar(",");
+		putchar(" ");
+		a_h1 = b_h1;
+		a_h2 = b_h2;
+		b_h1 = h1;
+		b_h2 = h2;
+
 	putchar('\n');
+	}
 
 	return (0);
 }
